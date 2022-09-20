2dc00865cc83771baf3adf7a08881322248b3948
Amit Mankotia <amit100.mankotia@gmail.com>
2022-09-20 21:24:09 +0530

    status

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -4,6 +4,13 @@ export default class Banner extends Component {
   render() {
     let movie=movies.results[0];
     return (
+      <>
+      {
+        movie.length==0?
+        <div class="spinner-border text-warning" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+        :
 <div className="card banner-card">
   <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} class="card-img-top banner-img" alt="..."/>
     <h5 className="card-title banner-title">{movie.original_title}</h5>
@@ -11,6 +18,8 @@ export default class Banner extends Component {
       {movie.overview}</p>
     {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
 </div>
+}
+</>
     )
   }
 }
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react'
 import { movies } from './getMovies'
 export default class List extends Component {
+  constructor(){
+    super()
+    this.state={
+      hover:"",
+    }
+  }
+  handleEnter=(id)=>{
+this.setState({
+  hover:id
+})
+  }
+  handleLeave=()=>{
+    this.setState({
+      hover:""
+    })
+  }
   render() {
     let allMovies= movies.results;
     return (
+      <>
+      {
+        allMovies.length==0?
+        <div class="spinner-border text-info" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+:
+<>
       <div >
         <h3 className='trending display-3'>Trending</h3>
         <div className='movies-list'>
           {allMovies.map((movieObj)=>{
             return (
-              <div className="card movie-card">
+              <div className="card movie-card" onMouseEnter={()=>{this.handleEnter(movieObj.id)}} onMouseLeave={this.handleLeave}>
             <img src={`https://image.tmdb.org/t/p/original/${movieObj.backdrop_path}`} class="card-img-top movie-img" alt="..." />
               <h5 className="card-title movie-title">{movieObj.original_title}</h5>
       <div className='button-wrapper'>
-              <a href="#" className="btn btn-primary movie-button">Add to Favourites</a>
+        {this.state.hover==movieObj.id &&
+              <a href="#" className="btn btn-primary movie-button">
+                Add to Favourites</a>
+              }
             </div>
           </div>
             )
@@ -21,6 +48,18 @@ export default class List extends Component {
           }
         </div>
       </div>
+      <nav aria-label="Page navigation example" className='pagination'>
+      <ul className="pagination">
+        <li className="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li className="page-item"><a class="page-link" href="#">1</a></li>
+        <li className="page-item"><a class="page-link" href="#">2</a></li>
+        <li className="page-item"><a class="page-link" href="#">3</a></li>
+        <li className="page-item"><a class="page-link" href="#">Next</a></li>
+      </ul>
+    </nav>
+    </>
+      }
+      </>
     )
   }
 }
