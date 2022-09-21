f4ffe95aadc9162b6743c66f6093811e0e9f8168
Amit Mankotia <amit100.mankotia@gmail.com>
2022-09-22 00:59:32 +0530

    batch

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -7,12 +7,12 @@ export default class Banner extends Component {
       <>
       {
         movie.length==0?
-        <div class="spinner-border text-warning" role="status">
-  <span class="visually-hidden">Loading...</span>
+        <div className="spinner-border text-warning" role="status">
+  <span className="visually-hidden">Loading...</span>
 </div>
         :
 <div className="card banner-card">
-  <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} class="card-img-top banner-img" alt="..."/>
+  <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} className="card-img-top banner-img" alt="..."/>
     <h5 className="card-title banner-title">{movie.original_title}</h5>
     <p className="card-text banner-text">
       {movie.overview}</p>
--- src/Components/List.js
+++ src/Components/List.js
@@ -9,22 +9,22 @@ export default class List extends Component {
   }
   handleEnter=(id)=>{
 this.setState({
-  hover:id
+  hover:id,
 })
-  }
+}
   handleLeave=()=>{
     this.setState({
-      hover:""
+      hover:"",
     })
   }
   render() {
-    let allMovies= movies.results;
-    return (
-      <>
+    let allMovies= movies.results; 
+    return (  
+   <> 
       {
         allMovies.length==0?
-        <div class="spinner-border text-info" role="status">
-  <span class="visually-hidden">Loading...</span>
+        <div className="spinner-border text-info" role="status">
+  <span className="visually-hidden">Loading...</span>
 </div>
 :
 <>
@@ -33,8 +33,8 @@ this.setState({
         <div className='movies-list'>
           {allMovies.map((movieObj)=>{
             return (
-              <div className="card movie-card" onMouseEnter={()=>{this.handleEnter(movieObj.id)}} onMouseLeave={this.handleLeave}>
-            <img src={`https://image.tmdb.org/t/p/original/${movieObj.backdrop_path}`} class="card-img-top movie-img" alt="..." />
+              <div className="card movie-card" onMouseEnter={()=>{this.handleEnter(movieObj.id)}} onMouseLeave={this.handleLeave} key={movieObj.id}>
+            <img src={`https://image.tmdb.org/t/p/original/${movieObj.backdrop_path}`} className="card-img-top movie-img" alt="..." />
               <h5 className="card-title movie-title">{movieObj.original_title}</h5>
       <div className='button-wrapper'>
         {this.state.hover==movieObj.id &&
@@ -50,11 +50,11 @@ this.setState({
       </div>
       <nav aria-label="Page navigation example" className='pagination'>
       <ul className="pagination">
-        <li className="page-item"><a class="page-link" href="#">Previous</a></li>
-        <li className="page-item"><a class="page-link" href="#">1</a></li>
-        <li className="page-item"><a class="page-link" href="#">2</a></li>
-        <li className="page-item"><a class="page-link" href="#">3</a></li>
-        <li className="page-item"><a class="page-link" href="#">Next</a></li>
+        <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+        <li className="page-item"><a className="page-link" href="#">1</a></li>
+        <li className="page-item"><a className="page-link" href="#">2</a></li>
+        <li className="page-item"><a className="page-link" href="#">3</a></li>
+        <li className="page-item"><a className="page-link" href="#">Next</a></li>
       </ul>
     </nav>
     </>
